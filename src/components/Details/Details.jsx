6c3848d13341340564cca66ea69ccca81e9d0ff5
nirobutso140 <nirobutso140@gmail.com>
2023-09-25 16:20:57 +0600

    create donate button

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,9 +1,10 @@
 const Details = ({donate}) => {
-    const {image, title, description} = donate
+    const {image, title, description, price, text_color} = donate
     return (
         <>
            <div className="details_data">
                 <img src={image} alt="" />
+                <button className="px-5 py-2 rounded-lg relative bottom-[60px] left-[20px]" style={{background: text_color, color: 'white'}}>Donate ${price}</button>
                 <p className="text-3xl font-bold my-5">{title}</p>
                 <p className="text-gray-600">{description}</p>
            </div>
--- src/components/Donate/Donate.jsx
+++ src/components/Donate/Donate.jsx
@@ -10,7 +10,7 @@ const Donate = ({donate}) => {
           <Link to={`/donationDetails/${id}`}>
             <div className="donate_card w-64 rounded-lg " style={{ backgroundColor: card_bg}}>
                     <img className="donate_img" src={picture} alt="" />
-                    <p className='px-2 py-1 mx-3 my-2 rounded-md w-[75px] text-center ' style={{ backgroundColor: category_bg, color: text_color}}>{category}</p>
+                    <p className='px-2 py-1 mx-3 my-2 rounded-md w-[90px] text-center ' style={{ backgroundColor: category_bg, color: text_color}}>{category}</p>
                     <p className=' mx-4 py-2' style={{color: text_color}}>{title}</p>
             </div>
           </Link>
