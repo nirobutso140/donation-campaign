89513960b0bc3f5733c3a38e5f748759f0a8aa6f
nirobutso140 <nirobutso140@gmail.com>
2023-09-25 12:33:03 +0600

    design layout to show donation category data

--- src/components/Donate/Donate.jsx
+++ src/components/Donate/Donate.jsx
@@ -1,10 +1,16 @@
-
+import './Donate.css'
 
 const Donate = ({donate}) => {
-    const {title} = donate
+    const {title, picture, category, category_bg, card_bg, text_color} = donate
+
+
     return (
         <>
-            <p>{title}</p>
+           <div className="donate_card w-64 rounded-lg " style={{ backgroundColor: card_bg}}>
+                <img className="donate_img" src={picture} alt="" />
+                <p className='px-2 py-1 mx-3 my-2 rounded-md w-[75px] text-center ' style={{ backgroundColor: category_bg, color: text_color}}>{category}</p>
+                <p className=' mx-4 py-2' style={{color: text_color}}>{title}</p>
+           </div>
         </>
     );
 };
--- src/components/Donets/Donets.jsx
+++ src/components/Donets/Donets.jsx
@@ -3,7 +3,7 @@ import Donate from "../Donate/Donate";
 const Donets = ({dontes}) => {
     return (
         <>
-          <div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 place-content-center gap-y-5">
               {
                 dontes?.map(donate => <Donate key={donate.id} donate={donate}/>)
               }
