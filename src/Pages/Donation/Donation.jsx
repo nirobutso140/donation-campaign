cd794bf3392027e9d571cfa80c61e2c298f3ca2d
nirobutso140 <nirobutso140@gmail.com>
2023-09-27 02:00:39 +0600

    set details image overlay

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -1,26 +1,32 @@
 import {useEffect, useState } from "react";
+import swal from 'sweetalert';
 import DonateItems from "../../components/DonateItems/DonateItems";
 
 const Donation = () => {
   
    const [donateItem, setDonateItem] = useState([])
-
+   const [noDataFound, setNoDataFound] = useState('')
 
     useEffect(()=>{
         const donationItems = JSON.parse(localStorage.getItem('item'))
-        setDonateItem(donationItems)
+        if(donationItems){
+            setDonateItem(donationItems)
+        }else{
+            setNoDataFound('Sorry No Data Found!!!')
+        }
+        
     }, [])
     
     
    
    
     return (
-        <>
+        <> 
+           
            <div className="grid grid-cols-1 md:grid-cols-2 place-content-center gap-5 p-5">
-              {
-                   donateItem.map(item=> <DonateItems key={item.id} item={item}/>)
-                  
-              }
+              
+                  {noDataFound? <p className="text-center w-[100%]">{noDataFound}</p> : 
+                  donateItem.map(item=> <DonateItems key={item.id} item={item}/>)} 
            </div>
            <button className="btn btn-success mx-auto block">See All</button>
         </>
--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,5 +1,4 @@
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import swal from 'sweetalert';
 
 const Details = ({donate}) => {
     const {id, image, title, description, price, text_color} = donate
@@ -10,14 +9,15 @@ const Details = ({donate}) => {
         if(!donationItems){
              addedItems.push(donate)
              localStorage.setItem('item',JSON.stringify(addedItems))
-             toast("Thanks For Your Donation!!");
+             swal("Good job!", "Thanks For your Donation!", "success");
         }else{
             const isExists = donationItems.find(myDonate => myDonate.id===id)
             if(!isExists){
                 addedItems.push(...donationItems , donate) 
                 localStorage.setItem('item',JSON.stringify(addedItems))
+                swal("Good job!", "Thanks For your Donation!", "success");
             }else{
-                toast.error("Already Doante");
+                swal("Error!", "You Already Donate This Item", "error");
             }
            
         }
@@ -29,10 +29,10 @@ const Details = ({donate}) => {
         <>
            <div className="details_data">
                 <img src={image} alt="" />
-                <button className="px-5 py-2 rounded-lg relative bottom-[60px] left-[20px]" style={{background: text_color, color: 'white'}} onClick={giveDonation}>Donate ${price}</button>
+                <div className='w-[100%] h-20 relative bottom-[80px] opacity-40 bg-green-950'></div>
+                <button className="px-5 py-2 rounded-lg relative bottom-[140px] left-[20px]" style={{background: text_color, color: 'white'}} onClick={giveDonation}>Donate ${price}</button>
                 <p className="text-3xl font-bold my-5">{title}</p>
                 <p className="text-gray-600">{description}</p>
-                <ToastContainer />
            </div>
         </>
     );
