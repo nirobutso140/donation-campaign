354a7254ed363adcc522015f692e702c0bea56fc
nirobutso140 <nirobutso140@gmail.com>
2023-09-26 08:55:01 +0600

    create banner section

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -14,7 +14,7 @@ const Donation = () => {
         <>
            <div className="grid grid-cols-1 md:grid-cols-2 place-content-center gap-5 p-5">
               {
-                donateItem.map(item=> <DonateItems item={item}/>)
+                donateItem.map(item=> <DonateItems key={item.id} item={item}/>)
               }
            </div>
            <button className="btn btn-success mx-auto block">See All</button>
